7224f36f55f534338f34e1422955231e3fe19e91
User Qrprat77 <qrprat77@HelloKitty.(none)>
2011-02-07 00:16:49 -0500

    updated libraries

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -12,28 +12,9 @@ module Codebreaker
 		end
 		
 		def guess(guess)
-			@output.puts '+' * exact_match_count(guess) + "-"* number_match_count(guess)
+			marker = Marker.new(@secret, guess)
+			@output.puts '+' * marker.exact_match_count + "-"* marker.number_match_count
 		end
 		
-		def exact_match_count(guess)
-			(0..3).inject(0) do |count, index|
-				count + (exact_match?(guess, index) ? 1 : 0)
-			end
-		end
-
-		def number_match_count(guess)
-			number_match_count = 0
-			(0..3).inject(0) do |count, index|
-				count +  (number_match?(guess, index) ? 1 : 0)
-			end #iteration over index
-		end
-
-		def exact_match?(guess, index)
-			guess[index] == @secret[index]
-		end #exact_match?
-		
-		def number_match?(guess, index)
-			@secret.include?(guess[index]) && !exact_match?(guess, index)
-		end #number_match?
-	end
+	end #game
 end
